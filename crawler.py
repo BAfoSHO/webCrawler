286b23a02ca64a74175ceeee5f1aebd0fc48f4ea
BA <bafosho@gmail.com>
2023-11-23 13:04:09 -0600

    update.04

--- crawler.py
+++ crawler.py
@@ -1,24 +1,40 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import WebDriverException, NoSuchElementException
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from export_cookies import export_cookies_to_csv
 import tldextract
 from database import create_connection, insert_cookie_data, insert_research_data
 from config import DATABASE_PATH
-import threading
+import sqlite3
+
+
+def check_for_duplicate(conn, table, url):
+    """ Check if the given URL already exists in the specified table """
+    try:
+        cur = conn.cursor()
+        sql = f"SELECT id FROM {table} WHERE url = ?"
+        cur.execute(sql, (url,))
+        return cur.fetchone() is not None
+    except sqlite3.Error as e:
+        print(f"Error checking for duplicates: {e}")
+        return False
 
 
 class Crawler:
     def __init__(self, headless=False):
         self.driver = None
         self.headless = headless
-        
+        self.conn = None
+
     def start_browser(self):
         options = webdriver.ChromeOptions()
         if self.headless:
             options.add_argument("--headless")
         try:
-            self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+            service = Service(ChromeDriverManager().install())
+            self.driver = webdriver.Chrome(service=service, options=options)
         except WebDriverException as e:
             print(f"Error starting the browser: {e}")
 
@@ -29,7 +45,7 @@ class Crawler:
     def navigate_to(self, url):
         try:
             self.driver.get(url)
-            time.sleep(5)
+            WebDriverWait(self.driver, 10).until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
         except WebDriverException as e:
             print(f"Error navigating to {url}: {e}")
 
@@ -39,7 +55,7 @@ class Crawler:
         except WebDriverException as e:
             print(f"Error getting cookies: {e}")
             return []
-        
+
     def categorize_cookies(self, base_url, cookies):
         base_domain = tldextract.extract(base_url).registered_domain
         first_party = []
@@ -61,41 +77,104 @@ class Crawler:
 
     def extract_page_data(self, url):
         self.navigate_to(url)
-        # Example extraction logic (needs to be tailored to your needs)
-        title = self.driver.find_element_by_tag_name('h1').text
-        content = self.driver.find_element_by_tag_name('body').text
-        return {'url': url, 'title': title, 'content': content}
+        try:
+            title = self.driver.find_element_by_tag_name('h1').text
+            content = self.driver.find_element_by_tag_name('body').text
+            return {'url': url, 'title': title, 'content': content}
+        except NoSuchElementException as e:
+            print(f"Error extracting data from {url}: {e}")
+            return {'url': url, 'title': '', 'content': ''}
 
-    def run(self, urls, mode='scrape', max_results=5):
-        conn = create_connection(DATABASE_PATH)
-        if conn is None:
+    def research_mode_logic(self, query, max_results):
+        search_results = self.google_search(query, max_results)
+        for url in search_results:
+            if not check_for_duplicate(self.conn, 'research_data', url):
+                try:
+                    page_data = self.extract_page_data(url)
+                    if page_data['title'] and page_data['content']:
+                        research_data = (query, page_data['url'], page_data['title'], page_data['content'])
+                        insert_research_data(self.conn, research_data)
+                except Exception as e:
+                    print(f"Error processing {url} in research mode: {e}")
+            else:
+                print(f"Duplicate found, skipping: {url}")
+
+    def scrape_mode_logic(self, urls):
+        for url in urls:
+            if not check_for_duplicate(self.conn, 'cookies', url):
+                try:
+                    self.navigate_to(url)
+                    cookies = self.get_cookies()
+                    first_party, third_party = self.categorize_cookies(url, cookies)
+                    for cookie in first_party + third_party:
+                        is_first_party = cookie['domain'] == tldextract.extract(url).registered_domain
+                        cookie_data = (url, cookie['name'], cookie['value'], cookie['domain'], 
+                                    cookie['path'], cookie.get('expiry'), cookie.get('httpOnly'), 
+                                    cookie.get('secure'), is_first_party)
+                        insert_cookie_data(self.conn, cookie_data)
+                except Exception as e:
+                    print(f"Error processing {url} in scrape mode: {e}")
+            else:
+                print(f"Duplicate found, skipping: {url}")
+
+    def export_cookies(self, csv_file_path):
+        export_cookies_to_csv(DATABASE_PATH, csv_file_path)
+
+    def run(self, input_data, mode='scrape', max_results=5, export_to_csv=False):
+        self.conn = create_connection(DATABASE_PATH)
+        if self.conn is None:
             print("Error! Cannot create a database connection.")
             return
 
         self.start_browser()
         if mode == 'research':
-            for term in urls:  # In research mode, urls are search terms
-                search_urls = self.google_search(term, max_results=max_results)
-                for url in search_urls:
-                    page_data = self.extract_page_data(url)
-                    insert_research_data(conn, (term, page_data['url'], page_data['title'], page_data['content']))
+            for query in input_data:
+                self.research_mode_logic(query, max_results)
         else:  # Scrape mode
-            for url in urls:
-                self.navigate_to(url)
-                cookies = self.get_cookies()
-                first_party, third_party = self.categorize_cookies(url, cookies)
-                for cookie in first_party + third_party:
-                    is_first_party = cookie['domain'] == tldextract.extract(url).registered_domain
-                    cookie_data = (url, cookie['name'], cookie['value'], cookie['domain'], 
-                                   cookie['path'], cookie.get('expiry'), cookie.get('httpOnly'), 
-                                   cookie.get('secure'), is_first_party)
-                    insert_cookie_data(conn, cookie_data)
+            self.scrape_mode_logic(input_data)
 
-        conn.close()
+        self.conn.close()
         self.quit_browser()
 
+        if export_to_csv:
+            self.export_cookies('cookies_export.csv')
+
+
+def display_help():
+    print("Crawler Help:")
+    print("  - 'scrape': Enter scrape mode to crawl specified URLs for cookies.")
+    print("  - 'research': Enter research mode to perform search queries and extract data from the results.")
+    print("  - 'q', 'quit', or 'exit': Quit the crawler.")
+    print("  - '-help': Display this help information.")
+
+
+def prepend_http(urls):
+    return [url if '://' in url else 'http://' + url for url in urls]
+
+
 if __name__ == "__main__":
-    # Example usage
     crawler = Crawler(headless=True)
-    crawler.run(["https://www.example.com"], mode='scrape')
+
+    while True:
+        command = input("Enter command ('-help' for options): ").strip().lower()
+
+        if command == '-help':
+            display_help()
+        elif command in ['q', 'quit', 'exit']:
+            print("Quitting the crawler.")
+            break
+        elif command == 'scrape':
+            urls = input("Enter URLs to scrape, separated by commas: ").split(',')
+            urls = prepend_http(urls)  # Prepend 'http://' if missing
+            crawler.run(urls, mode='scrape')
+        elif command == 'scrape':
+            urls = input("Enter URLs to scrape, separated by commas: ").split(',')
+            crawler.run(urls, mode='scrape')
+        elif command == 'research':
+            queries = input("Enter search queries, separated by commas: ").split(',')
+            crawler.run(queries, mode='research')
+        else:
+            print("Invalid command. Enter '-help' for options.")
+
+    print("Crawler has been stopped.")
 
--- database.py
+++ database.py
@@ -1,56 +1,55 @@
 import sqlite3
 from config import DATABASE_PATH
 
-db_file = DATABASE_PATH
 
-def create_connection():
-    """ Create a database connection to the SQLite database specified by db_file """
-    conn = None
+def create_connection(db_path):
+    """ Create a database connection to the SQLite database specified by db_path """
     try:
-        conn = sqlite3.connect(DATABASE_PATH)
+        conn = sqlite3.connect(db_path)
+        return conn
     except sqlite3.Error as e:
-        print(e)
-    return conn
+        print(f"Error creating database connection: {e}")
+        return None
 
 
 def create_table(conn, create_table_sql):
-    """ Create a table from the create_table_sql statement """
     try:
         c = conn.cursor()
         c.execute(create_table_sql)
+        conn.commit()
+        print("Table created successfully.")  # Debug print
     except sqlite3.Error as e:
-        print(e)
+        print(f"Error creating table: {e}")  # Error print
 
 
 def insert_cookie_data(conn, cookie_data):
     """ Insert a new row into the cookies table """
     sql = '''INSERT INTO cookies(url, name, value, domain, path, expires, http_only, secure, first_party)
               VALUES(?,?,?,?,?,?,?,?,?)'''
-    try:
-        cur = conn.cursor()
-        cur.execute(sql, cookie_data)
-        conn.commit()
-        return cur.lastrowid
-    except sqlite3.Error as e:
-        print(f"Error inserting cookie data: {e}")
+    with conn:
+        try:
+            cur = conn.cursor()
+            cur.execute(sql, cookie_data)
+            return cur.lastrowid
+        except sqlite3.Error as e:
+            print(f"Error inserting cookie data: {e}")
 
 
 def insert_research_data(conn, research_data):
     """ Insert a new row into the research_data table """
     sql = '''INSERT INTO research_data(search_term, url, title, content)
               VALUES(?,?,?,?)'''
-    try:
-        cur = conn.cursor()
-        cur.execute(sql, research_data)
-        conn.commit()
-        return cur.lastrowid
-    except sqlite3.Error as e:
-        print(f"Error inserting research data: {e}")
+    with conn:
+        try:
+            cur = conn.cursor()
+            cur.execute(sql, research_data)
+            return cur.lastrowid
+        except sqlite3.Error as e:
+            print(f"Error inserting research data: {e}")
 
 
-def init_db(db_file):
-    # SQL statement for creating the cookies table
-    sql_create_cookies_table = """
+def init_db():
+    cookies_table_sql = """
     CREATE TABLE IF NOT EXISTS cookies (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         url TEXT NOT NULL,
@@ -66,9 +65,7 @@ def init_db(db_file):
     );
     """
 
-
-    # SQL statement for creating the research_data table
-    sql_create_research_table = """
+    research_data_table_sql = """
     CREATE TABLE IF NOT EXISTS research_data (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         search_term TEXT NOT NULL,
@@ -79,13 +76,14 @@ def init_db(db_file):
     );
     """
 
-
-    # create a database connection
-    conn = create_connection(db_file)
-    if conn is not None:
-        # create cookies table and research_data table
-        create_table(conn, sql_create_cookies_table)
-        create_table(conn, sql_create_research_table)
+    conn = create_connection(DATABASE_PATH)  # Pass DATABASE_PATH as an argument
+    if conn:
+        create_table(conn, cookies_table_sql)
+        create_table(conn, research_data_table_sql)
         conn.close()
     else:
         print("Error! cannot create the database connection.")
+
+if __name__ == "__main__":
+    init_db()
+
